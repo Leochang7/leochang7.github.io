ce0b3bb30618070ea6175b81279152a0916a5e20
Leo <1093964909@qq.com>
2026-06-18 21:40:10 +0800

    feat(writing): add mdx math and code support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import { unified } from "@astrojs/markdown-remark";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
 
 export default defineConfig({
   site: "https://leochang7.github.io",
+  integrations: [mdx()],
+  markdown: {
+    processor: unified({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }),
+    shikiConfig: {
+      theme: "github-dark",
+      wrap: true,
+    },
+  },
 });
